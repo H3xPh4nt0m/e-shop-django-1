d425899f332b5bc255604eb2d1efcd5ea739bf00
mdashik123456 <mdashik1404@gmail.com>
2026-02-22 03:25:34 +0600

    account app

--- eshop/settings.py
+++ eshop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.category'
+    'apps.category',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
